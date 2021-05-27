0af370a4a1b6f8253ad876c156987574134b4f08
Nikita <nikitatorok002@gmail.com>
2021-05-27 21:49:44 +0700

    Make func, which return rand text

---
+++ src/libninja/randomtext.cpp
@@ -0,0 +1,21 @@
+#include <libninja/randomtext.hpp>
+
+int GetRandomNumber(int min, int max) {
+  static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
+  return static_cast<int>(rand() * fraction * (max - min + 1) + min);
+}
+
+int GetRandomText(std::string **_lines) {
+  srand(time(NULL));
+  int number_of_text = 0;
+  int count = 0;
+  std::string filename;
+  number_of_text = GetRandomNumber(1, 4);
+  filename = "texts/" + std::to_string(number_of_text) + ".txt";
+  count = GetStringsFromFileS(filename, _lines);
+  if (count == -1) {
+    std::cout << "Error! Could't open the " << filename << std::endl;
+    exit(3);
+  }
+  return count;
+}
---
+++ src/libninja/randomtext.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <ctime>
+#include <iostream>
+#include <libninja/readingfile.hpp>
+#include <string>
+
+int GetRandomNumber(int min, int max);
+int GetRandomText(std::string **_lines);
