f90bef4999c0f1ed80bab481413363b9ae6cb44e
Nikita <nikitatorok002@gmail.com>
2021-05-27 21:47:55 +0700

    Add mistake system

--- src/libninja/exercise.hpp
+++ src/libninja/exercise.hpp
@@ -2,8 +2,13 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <string>
+#include <unistd.h>
 using namespace sf;
+
+bool Keyhalding(sf::Event &event, int current_string,
+                long unsigned int current_letter, std::string lines[]);
 
 void Exercise(sf::Event &event, bool *print_correct_letter,
               long unsigned int *current_letter, std::string lines[],
-              std::string &utf88, int *current_string);
+              std::string &utf88, int *current_string, int *mistakes,
+              int *queue, int count);
