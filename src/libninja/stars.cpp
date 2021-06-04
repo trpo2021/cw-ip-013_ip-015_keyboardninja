52ed9defbd4cf801a5044dac1e31bd8acea6be27
Nikita <nikitatorok002@gmail.com>
2021-06-04 13:02:01 +0700

    Clang-formating

--- src/libninja/stars.cpp
+++ src/libninja/stars.cpp
@@ -1,14 +1,14 @@
 #include <libninja/stars.hpp>
 
-int stars (int mistakes, int time) 
+int stars(int mistakes, int time)
 {
-	if (mistakes > 45)
-		return 0;
-	else if (mistakes > 30)
-		return 1;
-	else if (mistakes > 15 && time > 15)
-		return 2;
-	else if (time > 25)
-		return 3;
-	return -1;		
+    if (mistakes > 45)
+        return 0;
+    else if (mistakes > 30)
+        return 1;
+    else if (mistakes > 15 && time > 15)
+        return 2;
+    else if (time > 25)
+        return 3;
+    return -1;
 }
